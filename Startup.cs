4525346d1dfe2a2a21b694e31009c28e91fbc479
Victor Villar <victorhugovillar@hotmail.com>
2021-05-28 21:34:53 -0400

    tercer deploy

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace App_Az303
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hola desde Visual Studio, prueba AZ-303!");
                 });
             });
         }
